540385e0b7a9865b4c54f59de1f51e8dff7f8a37
Michał Kukiełka <kukielka.michal@gmail.com>
2019-01-16 14:52:45 +0100

    gas assimilations

--- starcraft_AI.py
+++ starcraft_AI.py
@@ -1,6 +1,6 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
-from sc2.constants import NEXUS, PROBE, PYLON
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 from sc2.player import Bot, Computer
 
 class MyBot(sc2.BotAI):
@@ -8,8 +8,10 @@ class MyBot(sc2.BotAI):
     async def on_step(self, iteration):
         await self.distribute_workers()  
         await self.build_workers()
-        await self.build_pylons()
-
+        await self.build_pylons()  # pylons are protoss supply buildings
+        await self.expand()  # expand to a new resource area.
+        await self.build_assimilator()  # getting gas
+        
     async def build_workers(self):
         
         for nexus in self.units(NEXUS).ready.noqueue:
@@ -24,9 +26,29 @@ class MyBot(sc2.BotAI):
             if nexuses.exists:
                 if self.can_afford(PYLON):
                     await self.build(PYLON, near=nexuses.first)
+                    
+    async def expand():
+        
+        if self.units(NEXUS).amount < 2 and self.can_afford(NEXUS):
+            await.self.expand_now()
+            
+    async def build_assimilator():
+        for nexus in self.units(NEXUS).ready:
+            vaspenes = self.state.vaspene_geyser.closer_than(25.0, nexus)
+            for vaspene in vaspenes:
+                
+                if not self.can_afford(ASSIMILATOR):
+                    break
+                
+                worker = self.select_build_worker(vaspene.position)
+                
+                if worker is None:
+                    break
+                    
+                if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vaspene))
+
     
-
-
 
 
 run_game(maps.get("AbyssalReefLE"), [
