af9bdb320295dda225cde97c7e495a718c927dd5
MichalKukielka <kukielka.michal@gmail.com>
2019-01-20 21:16:07 +0100

    wierd intel commit

--- starcraft_AI.py
+++ starcraft_AI.py
@@ -1,8 +1,13 @@
+#  StarCraft2 - API
 import sc2
 import random
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from sc2.ids.unit_typeid import UnitTypeId
+
+#  deep learning
+import cv2
+import numpy as np
 
 
 class MyBot(sc2.BotAI):
@@ -20,7 +25,26 @@ class MyBot(sc2.BotAI):
         await self.build_assimilator()  # getting gas
         await self.offensive_force_buildings()
         await self.build_offensive_force()
+        await self.intel()  # DEEP LEARNING S**T
         await self.attack()  # basic attacking method
+
+    async def intel(self):
+        
+        game_data = np.zeros((self.game_info.map_size[1], self.game_info.map_size[0], 3), np.uint8)
+
+        #   drawing NEXUSES on a map
+
+        for nexus in self.units(UnitTypeId.NEXUS):
+            nexus_coor = nexus.position
+            cv2.circle(game_data, (int(nexus_coor[0]), int(nexus_coor[1])), 10, (0, 255, 0), -1)
+
+        #   flipping image to match coors
+        flipped = cv2.flip(game_data, 0)
+
+        #   resize - only to visualization
+        resized = cv2.resize(flipped, dsize = None, fx = 2, fy = 2)
+        cv2.imshow('Intel', resized)
+        cv2.waitKey(1)
 
 
     async def build_workers(self):
@@ -71,12 +95,12 @@ class MyBot(sc2.BotAI):
 
             #  building gateway otherwise
 
-            elif len(self.units(UnitTypeId.GATEWAY)) < ((self.iteration / self.ITERATIONS_PER_MINUTE)/2):
+            elif len(self.units(UnitTypeId.GATEWAY)) < 1:
                 if self.can_afford(UnitTypeId.GATEWAY) and not self.already_pending(UnitTypeId.GATEWAY):
                     await self.build(UnitTypeId.GATEWAY, near = pylon)
 
             if self.units(UnitTypeId.CYBERNETICSCORE).ready.exists:
-                if len(self.units(UnitTypeId.STARGATE)) < ((self.iteration / self.ITERATIONS_PER_MINUTE)/2):
+                if len(self.units(UnitTypeId.STARGATE)) < (self.iteration / self.ITERATIONS_PER_MINUTE):
                     if self.can_afford(UnitTypeId.STARGATE) and not self.already_pending(UnitTypeId.STARGATE):
                         await self.build(UnitTypeId.STARGATE, near = pylon)
 
@@ -84,10 +108,10 @@ class MyBot(sc2.BotAI):
     async def build_offensive_force(self):
 
         # build units for army - Stalkers, Voidrays
-        for gateway in self.units(UnitTypeId.GATEWAY).ready.noqueue:
-            if not self.units(UnitTypeId.STALKER).amount > self.units(UnitTypeId.VOIDRAY).amount:
-                if self.can_afford(UnitTypeId.STALKER) and self.supply_left > 0:
-                    await self.do(gateway.train(UnitTypeId.STALKER))
+        # for gateway in self.units(UnitTypeId.GATEWAY).ready.noqueue:
+        #     if not self.units(UnitTypeId.STALKER).amount > self.units(UnitTypeId.VOIDRAY).amount:
+        #         if self.can_afford(UnitTypeId.STALKER) and self.supply_left > 0:
+        #             await self.do(gateway.train(UnitTypeId.STALKER))
 
         for stargate in self.units(UnitTypeId.STARGATE).ready.noqueue:
             if self.can_afford(UnitTypeId.VOIDRAY) and self.supply_left > 0:
@@ -96,19 +120,24 @@ class MyBot(sc2.BotAI):
 
     async def attack(self):
 
-        aggressive_units = {UnitTypeId.STALKER: [15, 5],
-                            UnitTypeId.VOIDRAY: [8, 3]}
-
+        aggressive_units = {
+                            # UnitTypeId.STALKER: [15, 5],
+                            UnitTypeId.VOIDRAY: [8, 3]
+                            }
 
         for UNIT in aggressive_units:
-            if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
-                for s in self.units(UNIT).idle:
-                    await self.do(s.attack(self.find_target(self.state)))
+            for s in self.units(UNIT).idle:
+                await self.do(s.attack(self.find_target(self.state)))
 
-            elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
-                if len(self.known_enemy_units) > 0:
-                    for s in self.units(UNIT).idle:
-                        await self.do(s.attack(random.choice(self.known_enemy_units)))
+        # for UNIT in aggressive_units:
+        #     if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
+        #         for s in self.units(UNIT).idle:
+        #             await self.do(s.attack(self.find_target(self.state)))
+
+        #     elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
+        #         if len(self.known_enemy_units) > 0:
+        #             for s in self.units(UNIT).idle:
+        #                 await self.do(s.attack(random.choice(self.known_enemy_units)))
 
 
 
